5c1374642acb308a95d686fd598b49d4464a42a3
Roman Hauptvogel <roman.hauptvogel@rec-global.com>
2015-09-29 22:09:06 +0200

    Add login screen

---
+++ OnTheMap/UdacityConvenience.swift
@@ -0,0 +1,9 @@
+//
+//  UdacityConvenience.swift
+//  OnTheMap
+//
+//  Created by Roman Hauptvogel on 20/09/15.
+//  Copyright (c) 2015 Roman Hauptvogel. All rights reserved.
+//
+
+import Foundation
