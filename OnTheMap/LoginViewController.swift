5cf70a25d3e1f666a670f85ce6b563b40737313f
Roman Hauptvogel <roman.hauptvogel@rec-global.com>
2015-09-29 22:10:13 +0200

    Add login screen

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  OnTheMap
 //
 //  Created by Roman Hauptvogel on 10/09/15.
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    var appDelegate: AppDelegate!
+    var session: NSURLSession!
+    
+    @IBOutlet weak var EmailText: UITextField!
+    @IBOutlet weak var PasswordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+            
+        /* Get the app delegate */
+        appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+            
+        /* Get the shared URL session */
+        session = NSURLSession.sharedSession()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func loginButtonTouch(sender: AnyObject) {
 
+        UdacityClient.sharedInstance().postCreateSession(EmailText.text!, password: PasswordText.text!) { (success, errorString) in
+            if (success != nil) {
+                dispatch_async(dispatch_get_main_queue(), {
+                    let controller = self.storyboard!.instantiateViewControllerWithIdentifier("MapNavigationController") as! UINavigationController
+                    self.presentViewController(controller, animated: true, completion: nil)
+                })
+            } else {
+                dispatch_async(dispatch_get_main_queue(), {
+                    let alertController = UIAlertController(title: "Alert", message:
+                        errorString?.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
+                    alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+                    
+                    self.presentViewController(alertController, animated: true, completion: nil)
+                })
+            }
+        }
+    }
 }
 
--- OnTheMap/UdacityConstants.swift
+++ OnTheMap/UdacityConstants.swift
@@ -6,4 +6,79 @@
 //  Copyright (c) 2015 Roman Hauptvogel. All rights reserved.
 //
 
-import Foundation
+extension UdacityClient {
+    
+    // MARK: - Constants
+    struct Constants {
+        
+        // MARK: API Key
+        //static let ApiKey : String = "18bdcd8c987c465f9e12004d93dc73f4"
+        
+        // MARK: URLs
+        static let BaseURL : String = "http://www.udacity.com/api/"
+        static let BaseURLSecure : String = "https://www.udacity.com/api/"
+        //static let AuthorizationURL : String = "https://www.themoviedb.org/authenticate/"
+        
+    }
+    
+    // MARK: - Methods
+    struct Methods {
+        
+        // MARK: Session
+        static let Session = "session"
+        
+        /*
+        // MARK: Account
+        static let Account = "account"
+        static let AccountIDFavoriteMovies = "account/{id}/favorite/movies"
+        static let AccountIDFavorite = "account/{id}/favorite"
+        static let AccountIDWatchlistMovies = "account/{id}/watchlist/movies"
+        static let AccountIDWatchlist = "account/{id}/watchlist"
+        
+        // MARK: Authentication
+        static let AuthenticationTokenNew = "authentication/token/new"
+        static let AuthenticationSessionNew = "authentication/session/new"
+        
+        // MARK: Search
+        static let SearchMovie = "search/movie"
+        
+        // MARK: Config
+        static let Config = "configuration"
+        */
+    }
+    
+    // MARK: - URL Keys
+    struct URLKeys {
+        
+        static let UserID = "id"
+        
+    }
+    
+    // MARK: - Parameter Keys
+    struct ParameterKeys {
+        
+        static let ApiKey = "api_key"
+        static let SessionID = "session_id"
+        static let RequestToken = "request_token"
+        static let Query = "query"
+        
+    }
+    
+    // MARK: - JSON Body Keys
+    struct JSONBodyKeys {
+        static let Udacity = "udacity" 
+        static let Username = "username"
+        static let Password = "password"
+    }
+    
+    // MARK: - JSON Response Keys
+    struct JSONResponseKeys {
+        
+        // MARK: General
+        static let Account = "account"
+        static let Registered = "registered"
+        static let Status = "status"
+        static let Error = "error"
+        
+    }
+}
